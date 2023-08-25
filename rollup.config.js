243e91873e2ae90933c896cfe323881c53319225
hectorroberto.lopez <hectorroberto.lopez@bbva.com>
2023-08-25 13:19:23 -0600

    Fix target

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default {
     /** Minify JS, compile JS to a lower language target */
     esbuild({
       minify: true,
-      target: ['chrome64', 'firefox67', 'safari11.1'],
+      target: 'esnext',
     }),
     /** Bundle assets references via import.meta.url */
     importMetaAssets(),
