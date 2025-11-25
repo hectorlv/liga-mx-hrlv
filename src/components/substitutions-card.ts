c7ff426a39983de657862367373fd2cdac1ee9d1
Héctor López <39016098+hectorlv@users.noreply.github.com>
2025-11-24 20:56:36 -0600

    Updated CSS styles for substitution entry component to adjust layout for mobile view.

---
+++ src/components/substitutions-card.ts
@@ -0,0 +1,17 @@
+    @media (max-width: 600px) {
+        .substitution-entry {
+          flex-direction: column;
+          align-items: flex-start;
+        }
+
+        .substitution-entry > *:nth-child(1),
+        .substitution-entry > *:nth-child(2) {
+          display: inline-block;
+        }
+
+        .substitution-entry > *:nth-child(3),
+        .substitution-entry > *:nth-child(4),
+        .substitution-entry > *:nth-child(5) {
+          display: inline-block;
+        }
+    }
