a0fa5f796f6aeba2f2431b2252e83450395cbb4a
Héctor López <39016098+hectorlv@users.noreply.github.com>
2026-04-18 22:16:20 -0600

    Actualizar matches-page.ts

--- src/pages/matches-page.ts
+++ src/pages/matches-page.ts
@@ -637,7 +637,7 @@ export class MatchesPage extends LitElement {
           match.fecha.getMonth() === this.todayDate.getMonth() &&
           match.fecha.getDate() === this.todayDate.getDate());
       const noPlayIn = match.jornada !== 18 && match.jornada !== 19; // Solo torneo sin playin
-      return findTeam && findMatchDay && todayDate && onlyPlayOff && noPlayin;
+      return findTeam && findMatchDay && todayDate && onlyPlayOff && noPlayIn;
     });
   }
 
